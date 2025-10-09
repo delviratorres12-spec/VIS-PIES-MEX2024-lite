4b17c7d4e62c657161592d405e7740e9933f92d6
Diego Elvira <delviratorres12@gmail.com>
2025-10-09 00:46:05 -0600

    ...

--- app.R
+++ app.R
@@ -119,7 +119,7 @@ ui <- fluidPage(
                   "),
              br(),
              br(),
-             p(" - Visualizador elaborado por Diego Elvira -"),
+             p(" - Visualizador elaborado por Diego Elvira - "),
 HTML("[![DOI](https://zenodo.org/badge/1067623785.svg)](https://doi.org/10.5281/zenodo.17241011)"))
     ),
     
